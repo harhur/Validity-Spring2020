66bac8e7a579c86e42fb13864f93352289f74df9
harhur <11700278+harhur@users.noreply.github.com>
2019-10-05 16:09:18 -0400

    clean up code

--- src/main/java/spring2020/validity/ValidityApplication.java
+++ src/main/java/spring2020/validity/ValidityApplication.java
@@ -4,70 +4,47 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import info.debatty.java.stringsimilarity.*;
-import com.github.vickumar1981.stringdistance.util.*;
 import org.apache.commons.codec.language.DoubleMetaphone;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 @SpringBootApplication
 public class ValidityApplication {
-  public static void main(String[] args) throws IOException {
-    String csvFilepath = "./lib/normal.csv";
-    BufferedReader csvReader = new BufferedReader(new FileReader(csvFilepath));
 
-    // Use Metaphone and Levenshtein algorithms
-    LinkedHashMap<String, List<String>> metaphone_map = new LinkedHashMap<>();
-    DoubleMetaphone mp = new DoubleMetaphone();
+  // Use Metaphone and Levenshtein algorithms
+  private static LinkedHashMap<String, List<String>> metaphone_map = new LinkedHashMap<>();
+  private static DoubleMetaphone mp = new DoubleMetaphone();
 
-    String metaphone_words = "";
-    String levenshtein = "";
+  private static LinkedHashSet<String> non_duplicates = new LinkedHashSet<>();
+  // new duplicates map to prevent concurrent modification exception
+  private static LinkedHashSet<String> duplicates = new LinkedHashSet<>();
 
-    String row = csvReader.readLine(); // skip headers
-    while ((row = csvReader.readLine()) != null) {
-      String[] data = row.split(",");
-      String[] c_data = new String[data.length];
-
-      int clean_index = 0;
-      for (int i=1; i<data.length - 1; i++) { // start 1 to ignore ID
-        if (!data[i].equals("")) {
-          c_data[clean_index++] = data[i];
-        }
-      }
-
-      metaphone_words = c_data[1] + c_data[2] + c_data[3];
-      levenshtein = c_data[4] + c_data[5] + c_data[6] + c_data[7] + c_data[10] + c_data[11];
-
-      String mp_score = mp.doubleMetaphone(metaphone_words);
-      List<String> similar_words;
-      if (!metaphone_map.containsKey(mp_score)) {
-        similar_words = new ArrayList<>();
-      } else {
-        similar_words = metaphone_map.get(mp_score);
-      }
-      String properties = "";
-      for (String property : c_data) {
-        if (property != null) {
-          properties += property;
-        }
-      }
-      similar_words.add(properties);
-      metaphone_map.put(mp_score, similar_words);
+  public static void metaphoneAnalysis(String metaphone_words, String[] c_data) {
+    String mp_score = mp.doubleMetaphone(metaphone_words);
+    List<String> similar_words;
+    if (!metaphone_map.containsKey(mp_score)) {
+      similar_words = new ArrayList<>();
+    } else {
+      similar_words = metaphone_map.get(mp_score);
     }
+    String properties = "";
+    for (String property : c_data) {
+      if (property != null) {
+        properties += property;
+      }
+    }
+    similar_words.add(properties);
+    metaphone_map.put(mp_score, similar_words);
+  }
 
-    LinkedHashSet<String> non_duplicates = new LinkedHashSet<>();
-    // new duplicates map to prevent concurrent modification exception
-    LinkedHashSet<String> duplicates = new LinkedHashSet<>();
-
+  public static void levenShteinAnalysis() {
     // Levenshtein algoirhtm
     Levenshtein lv = new Levenshtein();
     for (Map.Entry<String, List<String>> entry : metaphone_map.entrySet()) {
@@ -84,18 +61,56 @@ public class ValidityApplication {
         non_duplicates.addAll(value);
       }
     }
-/*
-    // Test output for DUPLICATES
+  }
+
+  public static String output() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Possible duplicate values:");
+    for (String d : duplicates) {
+      sb.append(d);
+      sb.append("\n\n");
+    }
+    sb.append("\n\n Likely non-duplicate values");
+    for (String n : non_duplicates) {
+      sb.append(n);
+      sb.append("\n\n");
+    }
+    return sb.toString();
+  }
+
+  public static void main(String[] args) throws IOException {
+    String csvFilepath = "./lib/normal.csv";
+    BufferedReader csvReader = new BufferedReader(new FileReader(csvFilepath));
+    String metaphone_words;
+
+    String row = csvReader.readLine(); // skip headers
+    while ((row = csvReader.readLine()) != null) {
+      String[] data = row.split(",");
+      String[] c_data = new String[data.length];
+
+      int clean_index = 0;
+      for (int i=1; i<data.length - 1; i++) { // start 1 to ignore ID
+        if (!data[i].equals("")) {
+          c_data[clean_index++] = data[i];
+        }
+      }
+
+      metaphone_words = c_data[1] + c_data[2] + c_data[3];
+
+      metaphoneAnalysis(metaphone_words, c_data);
+    }
+
+    levenShteinAnalysis();
+
+    // Test output
     System.out.println("Duplicate Values:");
     for (String s : duplicates) {
         System.out.println(s);
     }
-
     System.out.println("Non-duplicate values");
     for (String s : non_duplicates) {
       System.out.println(s);
-    }*/
-
+    }
 
     SpringApplication.run(ValidityApplication.class, args);
   }
--- src/main/java/spring2020/validity/controllers/ValidityController.java
+++ src/main/java/spring2020/validity/controllers/ValidityController.java
@@ -1,7 +1,10 @@
 package spring2020.validity.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import spring2020.validity.ValidityApplication;
 
 // @PostMapping, @PutMapping, @DeleteMapping, @PatchMapping
 
@@ -9,6 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ValidityController {
   @GetMapping("/dashboard")
   public String showDashboard() {
-    return "This is the dashboard!";
+    ValidityApplication application = new ValidityApplication();
+    return application.output();
+  }
+
+  @RequestMapping("/home")
+  public String home() {
+    return "redirect:/templates/index.html";
   }
 }
